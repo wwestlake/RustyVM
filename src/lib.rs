8eb0bf0ed0bd8951398581216ebaa3fca611b1fb
Bill Westlake <wwestlake@lagdaemon.com>
2022-06-01 11:46:55 -0400

    Experiement 1 working

--- src/lib.rs
+++ src/lib.rs
@@ -39,10 +39,121 @@ pub enum Instruction {
     Div,                        
     Jmp(Box<Value>),            
     Halt,                       
-    Out(Box<Value>),            
+    Out(Box<Value>),
+    Movi(usize, Box<Value>),    // move imediate to register 
+                                // (usize reg 0-15 inclusive)            
+    Movr(usize, usize),         // move from reg a to reg b
     Dump,                       
 }
 
+#[derive(Clone)]
+#[derive(Debug)]
+struct Flags {
+    zero: bool,
+    pos: bool,
+    neg: bool
+}
+
+impl Flags {
+    fn new() -> Flags {
+        Flags {
+            zero: false,
+            pos: false,
+            neg: false
+        }
+    }
+}
+
+#[derive(Clone)]
+#[derive(Debug)]
+enum Register {
+    I32(i32),
+    I64(i64),
+    F32(f32),
+    F64(f64),
+    Char(char),
+    String(String),
+    Bool(bool),
+    Empty,
+}
+
+#[derive(Clone)]
+#[derive(Debug)]
+struct GeneralRegisters {
+    registers: Vec<Register>,
+}
+
+impl GeneralRegisters {
+    fn new() -> GeneralRegisters {
+        let mut result = GeneralRegisters { registers: vec![] };
+        for i in 0..16 {
+            result.registers.push(Register::Empty);
+        }
+        result
+    }
+
+    fn set(&mut self, reg: usize, value: Value) {
+        if reg > 15 {
+            panic!("Invalid register: {}", reg);
+        }
+
+        let val = match value {
+            Value::I32(v) => match v {
+                Some(x) => Register::I32(x),
+                None => Register::Empty
+            },
+            Value::I64(v) => match v {
+                Some(x) => Register::I64(x),
+                None => Register::Empty
+            },
+            Value::F32(v) => match v {
+                Some(x) => Register::F32(x),
+                None => Register::Empty
+            },
+            Value::F64(v) => match v {
+                Some(x) => Register::F64(x),
+                None => Register::Empty
+            },
+            Value::Char(v) => match v {
+                Some(x) => Register::Char(x),
+                None => Register::Empty
+            },
+            Value::String(v) => match v {
+                Some(x) => Register::String(x),
+                None => Register::Empty
+            },
+            Value::Bool(v) => match v {
+                Some(x) => Register::Bool(x),
+                None => Register::Empty
+            },
+            
+            _ => Register::Empty // may gen an error?
+        };
+
+        self.registers[reg] = val;
+    }
+
+    fn get(&mut self, reg: usize) -> Option<Value> {
+        if reg > 15 {
+            panic!("Invalid register: {}", reg);
+        }
+
+        let val = self.registers[reg].clone();
+        let result = match val {
+            Register::I32(v) => Some(Value::I32(Some(v))),
+            Register::I64(v) => Some(Value::I64(Some(v))),
+            Register::F32(v) => Some(Value::F32(Some(v))),
+            Register::F64(v) => Some(Value::F64(Some(v))),
+            Register::Char(ch) => Some(Value::Char(Some(ch))),
+            Register::String(s) => Some(Value::String(Some(s))),
+            Register::Bool(b) => Some(Value::Bool(Some(b))),
+            Register::Empty => None,
+        };
+
+        result
+    }     
+
+}
 
 #[derive(Debug)]
 struct RustyVM {
@@ -51,7 +162,9 @@ struct RustyVM {
     memory: Vec<Value>,
     stack: Vec<Value>,
     symbol_table: HashMap<String, Value>,
-    
+    heap: Vec<Value>,
+    flags: Flags,
+    registers: GeneralRegisters,
 }
 
 impl RustyVM {
@@ -61,7 +174,10 @@ impl RustyVM {
             memory: vec![],
             stack: vec![],
             symbol_table: HashMap::new(),
-            running: false
+            running: false,
+            heap: vec![],
+            flags: Flags::new(),
+            registers: GeneralRegisters::new(),
         }
     }
 
@@ -69,6 +185,7 @@ impl RustyVM {
         while self.running {
             let instruction = &self.memory[self.pc];
             match instruction {
+                // stack instructions
                 Value::Instruction(Instruction::Nop) => self.do_nop(),  
                 Value::Instruction(Instruction::Push(_)) => self.do_push(),
                 Value::Instruction(Instruction::Pop) => self.do_pop(),                        
@@ -78,18 +195,22 @@ impl RustyVM {
                 Value::Instruction(Instruction::Div) => self.do_div(),                        
                 Value::Instruction(Instruction::Jmp(address)) => self.do_jump(address.clone()),
 
+                // register instructions
+                Value::Instruction(Instruction::Movi(reg, v)) => self.do_movi(reg.clone(), *v.clone()),
+                Value::Instruction(Instruction::Movr(r1, r2)) => self.do_movr(r1.clone(), r2.clone()),
+                // system instructions
                 Value::Instruction(Instruction::Halt) => self.do_halt(),                       
                 Value::Instruction(Instruction::Out(_)) => self.do_out(),
                 Value::Instruction(Instruction::Dump) => self.do_dump(),
-                _ => { self.exception(String::from("Invalid instruction"))},
+                _ => self.exception(String::from(format!("Invalid instruction: {:#?} at {}", instruction, self.pc))),
             }
         }
     }
 
     fn exception(&mut self, msg: String) {
-        println!("Exception: {}", msg);
         self.do_dump();
         self.do_halt();
+        println!("Exception: {}", msg);
     } 
 
     fn do_nop(&mut self) {
@@ -113,21 +234,36 @@ impl RustyVM {
         self.pc += 1;
         let right = self.stack.pop().clone();
         let left = self.stack.pop().clone();
-        println!("{:#?} + {:#?}", left, right);
         match (left, right) {
             (Some(l), Some(r)) => { 
                 match (l, r) {
-                    (Value::I32(Some(l)), Value::I32(Some(r))) => {  
-                        self.stack.push(Value::I32(Some(l + r)));
+                    (Value::I32(Some(l)), Value::I32(Some(r))) => {
+                        let val = l + r;
+                        self.flags.zero = val == 0;
+                        self.flags.pos = val > 0;
+                        self.flags.neg = val < 0;
+                        self.stack.push(Value::I32(Some(val)));
                     }
                     (Value::I64(Some(l)), Value::I64(Some(r))) => {
-                        self.stack.push(Value::I64(Some(l + r)));
+                        let val = l + r;
+                        self.flags.zero = val == 0;
+                        self.flags.pos = val > 0;
+                        self.flags.neg = val < 0;
+                        self.stack.push(Value::I64(Some(val)));
                     }
                     (Value::F32(Some(l)), Value::F32(Some(r))) => {
-                        self.stack.push(Value::F32(Some(l + r)));
+                        let val = l + r;
+                        self.flags.zero = val == 0.0;
+                        self.flags.pos = val > 0.0;
+                        self.flags.neg = val < 0.0;
+                        self.stack.push(Value::F32(Some(val)));
                     }
                     (Value::F64(Some(l)), Value::F64(Some(r))) => {
-                        self.stack.push(Value::F64(Some(l + r)));
+                        let val = l + r;
+                        self.flags.zero = val == 0.0;
+                        self.flags.pos = val > 0.0;
+                        self.flags.neg = val < 0.0;
+                        self.stack.push(Value::F64(Some(val)));
                     }
                     (_,_) => { self.exception(String::from("Add: Cannot perform add on types supplied"))},
                 
@@ -141,21 +277,36 @@ impl RustyVM {
         self.pc += 1;
         let right = self.stack.pop().clone();
         let left = self.stack.pop().clone();
-        println!("{:#?} + {:#?}", left, right);
         match (left, right) {
             (Some(l), Some(r)) => { 
                 match (l, r) {
                     (Value::I32(Some(l)), Value::I32(Some(r))) => {  
-                        self.stack.push(Value::I32(Some(l - r)));
+                        let val = l - r;
+                        self.flags.zero = val == 0;
+                        self.flags.pos = val > 0;
+                        self.flags.neg = val < 0;
+                        self.stack.push(Value::I32(Some(val)));
                     }
                     (Value::I64(Some(l)), Value::I64(Some(r))) => {
-                        self.stack.push(Value::I64(Some(l - r)));
+                        let val = l - r;
+                        self.flags.zero = val == 0;
+                        self.flags.pos = val > 0;
+                        self.flags.neg = val < 0;
+                        self.stack.push(Value::I64(Some(val)));
                     }
                     (Value::F32(Some(l)), Value::F32(Some(r))) => {
-                        self.stack.push(Value::F32(Some(l - r)));
+                        let val = l - r;
+                        self.flags.zero = val == 0.0;
+                        self.flags.pos = val > 0.0;
+                        self.flags.neg = val < 0.0;
+                        self.stack.push(Value::F32(Some(val)));
                     }
                     (Value::F64(Some(l)), Value::F64(Some(r))) => {
-                        self.stack.push(Value::F64(Some(l - r)));
+                        let val = l - r;
+                        self.flags.zero = val == 0.0;
+                        self.flags.pos = val > 0.0;
+                        self.flags.neg = val < 0.0;
+                        self.stack.push(Value::F64(Some(val)));
                     }
                     (_,_) => { self.exception(String::from("Sub: Cannot perform Sub on types supplied"))},
                 
@@ -169,21 +320,36 @@ impl RustyVM {
         self.pc += 1;
         let right = self.stack.pop().clone();
         let left = self.stack.pop().clone();
-        println!("{:#?} + {:#?}", left, right);
         match (left, right) {
             (Some(l), Some(r)) => { 
                 match (l, r) {
                     (Value::I32(Some(l)), Value::I32(Some(r))) => {  
-                        self.stack.push(Value::I32(Some(l * r)));
+                        let val = l * r;
+                        self.flags.zero = val == 0;
+                        self.flags.pos = val > 0;
+                        self.flags.neg = val < 0;
+                        self.stack.push(Value::I32(Some(val)));
                     }
                     (Value::I64(Some(l)), Value::I64(Some(r))) => {
-                        self.stack.push(Value::I64(Some(l * r)));
+                        let val = l * r;
+                        self.flags.zero = val == 0;
+                        self.flags.pos = val > 0;
+                        self.flags.neg = val < 0;
+                        self.stack.push(Value::I64(Some(val)));
                     }
                     (Value::F32(Some(l)), Value::F32(Some(r))) => {
-                        self.stack.push(Value::F32(Some(l * r)));
+                        let val = l * r;
+                        self.flags.zero = val == 0.0;
+                        self.flags.pos = val > 0.0;
+                        self.flags.neg = val < 0.0;
+                        self.stack.push(Value::F32(Some(val)));
                     }
                     (Value::F64(Some(l)), Value::F64(Some(r))) => {
-                        self.stack.push(Value::F64(Some(l * r)));
+                        let val = l * r;
+                        self.flags.zero = val == 0.0;
+                        self.flags.pos = val > 0.0;
+                        self.flags.neg = val < 0.0;
+                        self.stack.push(Value::F64(Some(val)));
                     }
                     (_,_) => { self.exception(String::from("Mul: Cannot perform Mul on types supplied"))},
                 
@@ -197,21 +363,36 @@ impl RustyVM {
         self.pc += 1;
         let right = self.stack.pop().clone();
         let left = self.stack.pop().clone();
-        println!("{:#?} + {:#?}", left, right);
         match (left, right) {
             (Some(l), Some(r)) => { 
                 match (l, r) {
                     (Value::I32(Some(l)), Value::I32(Some(r))) => {  
-                        self.stack.push(Value::I32(Some(l / r)));
+                        let val = l / r;
+                        self.flags.zero = val == 0;
+                        self.flags.pos = val > 0;
+                        self.flags.neg = val < 0;
+                        self.stack.push(Value::I32(Some(val)));
                     }
                     (Value::I64(Some(l)), Value::I64(Some(r))) => {
-                        self.stack.push(Value::I64(Some(l / r)));
+                        let val = l / r;
+                        self.flags.zero = val == 0;
+                        self.flags.pos = val > 0;
+                        self.flags.neg = val < 0;
+                        self.stack.push(Value::I64(Some(val)));
                     }
                     (Value::F32(Some(l)), Value::F32(Some(r))) => {
-                        self.stack.push(Value::F32(Some(l / r)));
+                        let val = l / r;
+                        self.flags.zero = val == 0.0;
+                        self.flags.pos = val > 0.0;
+                        self.flags.neg = val < 0.0;
+                        self.stack.push(Value::F32(Some(val)));
                     }
                     (Value::F64(Some(l)), Value::F64(Some(r))) => {
-                        self.stack.push(Value::F64(Some(l / r)));
+                        let val = l / r;
+                        self.flags.zero = val == 0.0;
+                        self.flags.pos = val > 0.0;
+                        self.flags.neg = val < 0.;
+                        self.stack.push(Value::F64(Some(val)));
                     }
                     (_,_) => { self.exception(String::from("Div: Cannot perform div on types supplied"))},
                 
@@ -226,8 +407,20 @@ impl RustyVM {
             Value::Address(Some(addr)) => { self.pc = addr },
             _ => {}
         }
+    }
 
+    fn do_movi(&mut self, reg: usize, val: Value) {
+        println!("{:#?}, {:#?}", reg, val);
+        self.registers.set(reg, val);
+        self.pc += 1;
+    }
 
+    fn do_movr(&mut self, reg1: usize, reg2: usize) {
+        match self.registers.get(reg2) {
+            Some(v) => self.registers.set(reg1, v),
+            None => self.exception(format!("cannot move empty register: {} is empty at this point", reg2)),
+        };
+        self.pc += 1;
     }
 
     fn do_out(&mut self) {
@@ -243,7 +436,6 @@ impl RustyVM {
 
     fn do_halt(&mut self) {
         self.running = false;
-        self.do_dump();
     }
 
 
@@ -328,6 +520,18 @@ impl VmBuilder {
                 self.pc += 1;
              },
         }
+        self
+    }
+
+    pub fn movi(&mut self, reg: usize, val: Value) -> &mut Self {
+        self.pc += 1;
+        self.vm.memory.push(Value::Instruction(Instruction::Movi(reg, Box::new(val))));
+        self
+    }
+
+    pub fn movr(&mut self, reg1: usize, reg2: usize) -> &mut Self {
+        self.pc += 1;
+        self.vm.memory.push(Value::Instruction(Instruction::Movr(reg1, reg2)));
         self
     }
 
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,13 @@ fn main() {
 
     vm_builder
         .label(String::from("Start"))
-        .dump()
-        .jump(String::from("End"))
+        .movi(0, Value::F32(Some(0.6)))
+        .movi(2, Value::F32(Some(2.6)))
+        .movi(4, Value::F32(Some(4.6)))
+        .movi(6, Value::F32(Some(6.6)))
+        .movi(8, Value::F32(Some(8.6)))
+        .movi(15, Value::F32(Some(15.6)))
+        .movr(3,2)
         .nop()
         .push(Value::F32(Some(3.14)))
         .push(Value::F32(Some(2.6)))
