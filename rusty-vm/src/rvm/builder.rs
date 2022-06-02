53f4417354778a9e97a17af93bfb46a2612ca7e8
Bill Westlake <wwestlake@lagdaemon.com>
2022-06-02 04:57:40 -0400

    Instruiction set started, runner working

--- rusty-vm/src/rvm/builder.rs
+++ rusty-vm/src/rvm/builder.rs
@@ -7,13 +7,14 @@ use std::{
     },
 };
 
-use super::vm::RustyVM;
+use super::vm::{RustyVM, MemoryCell, Instruction, Value};
 
 
 pub struct VMBuilder {
     vm: RustyVM, 
     pc: usize,
     symbol_table: HashMap<String, usize>,
+    built: bool
 }
 
 impl VMBuilder {
@@ -21,7 +22,49 @@ impl VMBuilder {
         VMBuilder {
             vm: RustyVM::new(),
             pc: 0,
-            symbol_table: HashMap::new()
+            symbol_table: HashMap::new(),
+            built: false
         }
     }
+
+    pub fn start(&mut self) {
+        if ! self.built {
+            println!("You must call build before start");
+            return
+        }
+        self.vm.reset();
+        self.vm.run();
+    }
+
+    pub fn build(&mut self) -> &mut Self {
+        // reconciles all labels here
+
+        self.built = true;
+        self
+    }
+
+    pub fn push(&mut self, val: Value) -> &mut Self {
+        self.vm.push(MemoryCell::Instruction(Instruction::Push(val)));
+        self.pc += 1;
+        self
+    }
+
+    pub fn add(&mut self) -> &mut Self {
+        self.vm.push(MemoryCell::Instruction(Instruction::Add));
+        self.pc += 1;
+        self
+    }
+
+    pub fn halt(&mut self) -> &mut Self {
+        self.vm.push(MemoryCell::Instruction(Instruction::Halt));
+        self.pc += 1;
+        self
+    }
+
+    pub fn dump(&mut self) -> &mut Self {
+        self.vm.push(MemoryCell::Instruction(Instruction::Dump));
+        self.pc += 1;
+        self
+    }
+
 }
--- rusty-vm/src/rvm/vm.rs
+++ rusty-vm/src/rvm/vm.rs
@@ -11,8 +11,7 @@ use std::{
 
 
 /// Values that the system is able to process
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Value {
     I32(i32),
     I64(i64),
@@ -27,8 +26,7 @@ pub enum Value {
 
 
 /// Instructions that the virtual machne will execute
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Instruction {
     Nop,                        
     Push(Value),           
@@ -43,16 +41,14 @@ pub enum Instruction {
     Dump,                       
 }
 
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum MetaData {
     Tag(String),
 
 }
 
 /// Describes what can be stored in a memory location
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum MemoryCell {
     Instruction(Instruction),
     Value(Value),
@@ -61,14 +57,17 @@ pub enum MemoryCell {
 }
 
 /// The virtual machine
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct RustyVM {
     pc: usize, // program counter
     running: bool,
     memory: Vec<MemoryCell>,
     stack: Vec<MemoryCell>,
     registers: Vec<MemoryCell>,
+
+    // special registers
+    cur_instruction: Option<Instruction>
+
 }
 
 impl RustyVM {
@@ -80,6 +79,7 @@ impl RustyVM {
             stack: vec![],
             registers: vec![],
             running: false,
+            cur_instruction: None
         };
         for i in 0..16 {
             vm.registers.push(MemoryCell::Empty)
@@ -87,7 +87,103 @@ impl RustyVM {
         vm
     }
 
+    fn handle_exception(&mut self, msg: &str) {
+        println!("Exception({}): {}\nCurrent Instruction\n{:#?}", self.pc, msg, self.cur_instruction);
+        self.ex_dump();
+        self.running = false;
+    }
+
+    pub fn push(&mut self, mem: MemoryCell) {
+        self.memory.push(mem);
+    }
+
+    pub fn reset(&mut self) {
+        self.pc = 0;
+        self.running = true;
+    }
+
+    fn fetch(&mut self) {
+        let cell = &self.memory[self.pc];
+
+        match cell {
+            MemoryCell::Instruction(i) => self.cur_instruction = Some(i.clone()),
+            _ => self.handle_exception("Invalid Instruction in MemoryCell")
+        };
+    }
+
+    fn decode(&mut self) {
+        match self.cur_instruction.clone() {
+            Some(inst) => self.execute(inst),
+            None => self.handle_exception("Invalid Instruction in MemoryCell")
+        };
+    }
+
+    fn execute(&mut self, inst: Instruction) {
+        match inst {
+            Instruction::Nop => self.pc += 1,
+            Instruction::Add => self.ex_add(),
+            Instruction::Push(x) => self.ex_push(x),
+            Instruction::Pop => self.ex_pop(),
+            Instruction::Dump => self.ex_dump(),
+            Instruction::Halt => self.ex_halt(),
+            _ => {}
+        };
+    }
+
+    fn ex_add(&mut self) {
+        let right = self.stack.pop();
+        let left = self.stack.pop();
+        println!("ex_add: {:#?} + {:#?}", left, right);
+        match (left, right) {
+            (Some(MemoryCell::Value(Value::I32(l))), 
+                Some(MemoryCell::Value(Value::I32(r)))) =>
+                self.stack.push(MemoryCell::Value( Value::I32(l + r)) ),
+            (Some(MemoryCell::Value(Value::I64(l))), 
+                Some(MemoryCell::Value(Value::I64(r)))) =>
+                self.stack.push(MemoryCell::Value( Value::I64(l + r)) ),
+
+            (Some(MemoryCell::Value(Value::F32(l))), 
+                Some(MemoryCell::Value(Value::F32(r)))) =>
+                self.stack.push(MemoryCell::Value( Value::F32(l + r)) ),
+            (Some(MemoryCell::Value(Value::F64(l))), 
+                Some(MemoryCell::Value(Value::F64(r)))) =>
+                self.stack.push(MemoryCell::Value( Value::F64(l + r)) ),
+
+            (l,r) => {
+                self.handle_exception(format!("Add: left and right operands must be of the same type: {:#?} + {:#?}", l, r).as_str())
+            }
+        }
+        self.pc += 1;
+    }
+
+    fn ex_push(&mut self, value: Value) {
+        self.stack.push(MemoryCell::Value( value ));
+        self.pc += 1;
+    }
+
+    fn ex_pop(&mut self) {
+        self.stack.pop();
+        self.pc += 1;
+    }
+
+
+    fn ex_halt(&mut self) {
+        self.running = false;
+    }
+
+    fn ex_dump(&mut self) {
+        println!("\n----------- Start Processor Dump -------------");
+        println!("{:#?}", self);
+        println!("\n----------- End Processor Dump -------------");
+
+        self.pc += 1;
+    }
+
     pub fn run(&mut self) {
+        while self.running {
+            self.fetch();
+            self.decode();
+        }
     }
 }
 
